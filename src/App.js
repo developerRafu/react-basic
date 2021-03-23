bf879c5ead1d87eccd93dd982b37c6b33382ea2c
rafael <fel.rique71@gmail.com>
2021-03-23 11:08:34 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
+class App extends Component {
+    state = {
+      name: 'Rafu',
+      counter: 0
+    };
+
+  handlePClick = () => {
+    this.setState({ name: 'rafs' })
+    console.log('p clicado')
+  }
+
+  handleAClick = (event) => {
+    event.preventDefault();
+    const { counter } = this.state;
+    this.setState({ counter: counter + 1 })
+  }
+
+  render() {
+    const { name, counter } = this.state;
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p onClick={this.handlePClick}>
+            {name} {counter}
+          </p>
+          <a
+            onClick={this.handleAClick}
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+/*function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Olá mundo
         </p>
         <a
           className="App-link"
@@ -20,6 +63,6 @@ function App() {
       </header>
     </div>
   );
-}
+}*/
 
 export default App;
